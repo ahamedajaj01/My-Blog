e55920800dcadd03743ac05410284d7112fa3af7
ahamedajaj01 <ahamed.ajaj01@gmail.com>
2025-07-19 20:33:16 +0545

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/posts" element={<Posts />} />
         <Route path="/posts/readPost/:id" element={<ReadPost />} />
-        <Route path="reset-password" element={<ResetPasswordPage/>} />
+        <Route path="/reset-password" element={<ResetPasswordPage/>} />
         
         <Route path="/signup" element={<PublicRoute><Signup /></PublicRoute>} />
 
